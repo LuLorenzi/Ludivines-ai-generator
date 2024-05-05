0cad3f64cee34a62733d92258c02da0a18be77a3
LuLorenzi <156010623+LuLorenzi@users.noreply.github.com>
2024-05-05 15:44:22 +0100

    updated AI prompt

--- src/script.js
+++ src/script.js
@@ -11,8 +11,7 @@ function generateHaiku(event) {
   event.preventDefault();
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "fb7f9ao5305aa93b380c3d2e8366342t";
-  let context =
-    `You are a haïku expert. Your mission is to write a great haïku in basic HTML and separate each line with a <br />. Include only your response. Sign the poem with "From the mind of the artificial" in <strong> at the bottom. Make sure to use the user instructions`;
+  let context = `You are a haïku expert. Your mission is to write a haïku in basic HTML, separating each line with a <br/>. Do not add a title. Sign the poem with "From the mind of the artificial" in <strong> at the bottom. Make sure to follow the user instructions`;
   let prompt = `User intructions: Generate a haïku about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
